a1e302b4b5c9a9b2e34ee30f55edce712ad5a9b6
ersoykadir <eersooy61@gmail.com>
2023-08-07 12:52:04 +0300

    server, get/post endpoints

---
+++ app/server/main.py
@@ -0,0 +1,39 @@
+# Following this tutorial: https://fastapi.tiangolo.com/tutorial/sql-databases/
+
+from typing import List
+
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from database import crud, models, schemas
+from database.db import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/items/", response_model=schemas.Item)
+def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    return crud.create_item(db=db, item=item)
+
+
+@app.get("/items/", response_model=schemas.Item)
+def read_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = crud.get_item(db, item_id=item_id)
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return db_item
+
+import uvicorn, os
+
+if __name__ == "__main__":
+   uvicorn.run("main:app", host=os.environ.get('HOST'), port=int(os.environ.get('PORT')), reload=True)
