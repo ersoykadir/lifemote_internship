32bc1e2b3fe80065b33060717d6e160d4f3a4131
ersoykadir <eersooy61@gmail.com>
2023-08-07 12:49:12 +0300

    stop sending req after success

--- app/client/main.py
+++ app/client/main.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
             # x = requests.get(f'http://server:{os.environ.get("SERVER_PORT")}/items/?item_id=1')
             y = requests.post(f'http://server:3000/items/', json = {'message':'test'})
             print(y.text)
+            break
         except Exception as e:
             print('Error connecting to server')
             print(e)
