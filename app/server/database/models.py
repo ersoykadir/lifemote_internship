ab06ab214c0bc8fa4051010864cbf6dd7a4aadae
ersoykadir <eersooy61@gmail.com>
2023-08-08 14:39:47 +0300

    models updated with completed column

--- app/server/database/models.py
+++ app/server/database/models.py
@@ -5,7 +5,8 @@ from .db import Base
 
 
 class Item(Base):
-    __tablename__ = "items"
+    __tablename__ = "item"
 
     id = Column(Integer, primary_key=True, index=True)
-    message = Column(String(64), unique=False, index=True)
+    message = Column(String(64), unique=False, index=True, nullable=False)
+    completed = Column(Boolean, default=False, nullable=False)
