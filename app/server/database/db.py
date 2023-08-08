204346d8c4fcc16c8dc1513f56961199a9083d4c
ersoykadir <eersooy61@gmail.com>
2023-08-08 14:37:26 +0300

    started using .env for testing

--- app/server/database/db.py
+++ app/server/database/db.py
@@ -3,10 +3,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 import os
-host = os.environ['MYSQL_HOST']
-user = os.environ['MYSQL_USER']
-password = os.environ['MYSQL_PASSWORD']
-db = os.environ['MYSQL_DB']
+from dotenv import load_dotenv
+load_dotenv()
+host = os.getenv('MYSQL_HOST')
+user = os.getenv('MYSQL_USER')
+password = os.getenv('MYSQL_PASSWORD')
+db = os.getenv('MYSQL_DB')
+# host = os.environ['MYSQL_HOST']
+# user = os.environ['MYSQL_USER']
+# password = os.environ['MYSQL_PASSWORD']
+# db = os.environ['MYSQL_DB']
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{user}:{password}@{host}:3306/{db}"
 
