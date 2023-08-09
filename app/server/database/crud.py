775e15aff0b68151277f4c2cc6bf4458eb1a0355
ersoykadir <eersooy61@gmail.com>
2023-08-09 12:40:56 +0300

    get all items endpoint

--- app/server/database/crud.py
+++ app/server/database/crud.py
@@ -6,6 +6,9 @@ from . import models, schemas
 def get_item(db: Session, item_id: int):
     return db.query(models.Item).filter(models.Item.id == item_id).first()
 
+def get_items(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Item).offset(skip).limit(limit).all()
+
 def create_item(db: Session, item: schemas.ItemCreate):
     db_item = models.Item(message=item.message)
     db.add(db_item)
--- app/server/main.py
+++ app/server/main.py
@@ -34,6 +34,11 @@ def read_item(item_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Item not found")
     return db_item
 
+@app.get("/items/", response_model=List[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
+
 @app.put("/items/{item_id}", response_model=schemas.Item, status_code=status.HTTP_200_OK)
 def update_item(item_id: int, completed: bool, db: Session = Depends(get_db)):
     db_item = crud.get_item(db, item_id=item_id)
