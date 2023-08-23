e5dcccd2ecf9f05488960b4d94b0c931eebfd4ef
ersoykadir <eersooy61@gmail.com>
2023-08-23 11:16:46 +0300

    get all contexts and get context by name

--- app/server/routers/contexts.py
+++ app/server/routers/contexts.py
@@ -14,6 +14,19 @@ router = APIRouter(
 )
 
 
+@router.get("/all", response_model=List[schemas.Context])
+def read_contexts_for_user(
+    db: Session = Depends(get_db), user: schemas.User = Depends(get_current_user)
+):
+    db_user = crud.user.get_user(db, user_id=user.id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db_contexts = crud.context.get_contexts_by_user(db, user_id=user.id)
+    if db_contexts is None:
+        raise HTTPException(status_code=404, detail="Contexts not found")
+    return db_contexts
+
+
 @router.get("/{context_id}", response_model=schemas.Context)
 def read_context(
     context_id: int,
@@ -21,6 +34,20 @@ def read_context(
     user: schemas.User = Depends(get_current_user),
 ):
     db_context = crud.context.get_context(db, context_id=context_id)
+    if db_context is None:
+        raise HTTPException(status_code=404, detail="Context not found")
+    return db_context
+
+
+@router.get("/", response_model=schemas.Context)
+def get_context_by_name(
+    context_name: str,
+    db: Session = Depends(get_db),
+    user: schemas.User = Depends(get_current_user),
+):
+    db_context = crud.context.get_context_by_name_for_user(
+        db, context_name=context_name, user_id=user.id
+    )
     if db_context is None:
         raise HTTPException(status_code=404, detail="Context not found")
     return db_context
@@ -39,15 +66,3 @@ def create_context_for_user(
         raise HTTPException(status_code=400, detail="Context already exists")
     return crud.context.create_context(db=db, context=context, user_id=user.id)
 
-
-@router.get("/", response_model=List[schemas.Context])
-def read_contexts_for_user(
-    db: Session = Depends(get_db), user: schemas.User = Depends(get_current_user)
-):
-    db_user = crud.user.get_user(db, user_id=user.id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    db_contexts = crud.context.get_contexts_by_user(db, user_id=user.id)
-    if db_contexts is None:
-        raise HTTPException(status_code=404, detail="Contexts not found")
-    return db_contexts
