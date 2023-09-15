5a128b9d9ff7d743acae1f125fc981b5eab5f4c3
ersoykadir <eersooy61@gmail.com>
2023-09-15 19:24:35 +0300

    wait for it timeout shortened

--- app/client/entrypoint.sh
+++ app/client/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 
 # Wait for the backend to be up, if we know where it is.
 if [ -n "$SERVER_HOST" ]; then
-  ./wait-for-it.sh $SERVER_HOST:$SERVER_PORT
+  ./wait-for-it.sh -t 2 $SERVER_HOST:$SERVER_PORT
 fi
 
 # Run the main container command.
