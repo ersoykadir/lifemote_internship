04cc9257d16622cba233b647914f23e8abc3b94a
ersoykadir <eersooy61@gmail.com>
2023-09-15 10:59:37 +0300

    alpine packages has no bash, using sh

---
+++ app/client/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Abort on any error (including if wait-for-it fails).
+set -e
+
+# Wait for the backend to be up, if we know where it is.
+if [ -n "$SERVER_HOST" ]; then
+  ./wait-for-it.sh $SERVER_HOST:$SERVER_PORT
+fi
+
+# Run the main container command.
+python main.py
