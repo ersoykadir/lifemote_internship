ff7fcbce1e3dec97d429cc7ce1db022d8d9a178d
ersoykadir <eersooy61@gmail.com>
2023-09-19 20:14:27 +0300

    declarative base module moved to another package

--- app/server/database/db.py
+++ app/server/database/db.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 from fastapi import HTTPException
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import OperationalError
 
