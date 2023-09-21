152a1d6cac268391475778a79020c1a6027740a6
ersoykadir <eersooy61@gmail.com>
2023-09-21 15:45:39 +0300

    sort the list from dependency function test, data moved to another file

--- app/server/tests/test_extras.py
+++ app/server/tests/test_extras.py
@@ -13,103 +13,44 @@ backend_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, backend_path)
 
 from main import app
+from routers import extras
+import extras_data
 
 load_dotenv()
 client = TestClient(app)
 
-# For now, I will use the real database for testing
-# TODO: Try to mock the database
 
-TOKEN = os.environ.get("ACCESS_TOKEN")
-print(TOKEN)
-HEADERS = {
-    "Authorization": "Bearer {TOKEN}"
-}
+def override_get_context_items():
+    """Dependency"""
+    return extras_data._ITEMS1
+
+def override_get_context_items_empty():
+    """Dependency"""
+    return []
+
+def override_get_context_items_nonlist():
+    """Dependency"""
+    return "Hello World"
 
 def test_sort_items():
     """ Test sort items"""
     payload = {
-        "items": [
-            {
-                "message": "Hello World",
-                "completed": False,
-                "context_name": "To-Do"
-            },
-            {
-                "message": "Hello Mars",
-                "completed": False,
-                "context_name": "To-Do"
-            }
-        ],
+        "items": extras_data.ITEMS1,
         "property_name": "message"
     }
-    response = client.post("/extras/sort_items", json=payload, headers=HEADERS)
+    response = client.post("/extras/sort_items", json=payload, headers=extras_data.HEADERS)
     assert response.status_code == 200
-    assert response.json() == [
-        {
-            "message": "Hello Mars",
-            "completed": False,
-            "context_name": "To-Do"
-        },
-        {
-            "message": "Hello World",
-            "completed": False,
-            "context_name": "To-Do"
-        }
-    ]
+    assert response.json() == extras_data.ITEMS1_SORTED
 
 def test_append_lists():
     """ Test append lists"""
     payload = {
-        "items1": [
-            {
-                "message": "Hello World",
-                "completed": False,
-                "context_name": "To-Do"
-            },
-            {
-                "message": "Hello Mars",
-                "completed": False,
-                "context_name": "To-Do"
-            }
-        ],
-        "items2": [
-            {
-                "message": "Hello Venus",
-                "completed": False,
-                "context_name": "To-Do"
-            },
-            {
-                "message": "Hello Mercury",
-                "completed": False,
-                "context_name": "To-Do"
-            }
-        ]
+        "items1": extras_data.ITEMS1,
+        "items2": extras_data.ITEMS2
     }
-    response = client.post("/extras/append_lists", json=payload, headers=HEADERS)
+    response = client.post("/extras/append_lists", json=payload, headers=extras_data.HEADERS)
     assert response.status_code == 200
-    assert response.json() == [
-        {
-            "message": "Hello World",
-            "completed": False,
-            "context_name": "To-Do"
-        },
-        {
-            "message": "Hello Mars",
-            "completed": False,
-            "context_name": "To-Do"
-        },
-        {
-            "message": "Hello Venus",
-            "completed": False,
-            "context_name": "To-Do"
-        },
-        {
-            "message": "Hello Mercury",
-            "completed": False,
-            "context_name": "To-Do"
-        }
-    ]
+    assert response.json() == extras_data.ITEMS1 + extras_data.ITEMS2
 
 def test_filter_list():
     """ Test filter list"""
@@ -118,22 +59,52 @@ def test_filter_list():
             {
                 "message": "Hello World",
                 "completed": False,
-                "context_name": "To-Do"
             },
             {
                 "message": "Hello Mars",
                 "completed": True,
-                "context_name": "To-Do"
             }
         ],
         "completed": False
     }
-    response = client.post("/extras/filter_list", json=payload, headers=HEADERS)
+    response = client.post("/extras/filter_list", json=payload, headers=extras_data.HEADERS)
     assert response.status_code == 200
     assert response.json() == [
         {
             "message": "Hello World",
             "completed": False,
-            "context_name": "To-Do"
         }
     ]
+
+def test_sort_itemsv1():
+    """ Test sort items"""
+    payload = {
+        "property_name": "message",
+        "context_id" : 4
+    }
+    app.dependency_overrides[extras.get_context_items] = override_get_context_items
+    response = client.post("/extras/sort_itemsv1", json=payload, headers=extras_data.HEADERS)
+    assert response.status_code == 200
+    assert response.json() == extras_data.ITEMS1_SORTED
+
+def test_sort_itemsv1_emptylist():
+    """ Test sort items"""
+    payload = {
+        "property_name": "message",
+        "context_id" : 4
+    }
+    app.dependency_overrides[extras.get_context_items] = override_get_context_items_empty
+    response = client.post("/extras/sort_itemsv1", json=payload, headers=extras_data.HEADERS)
+    assert response.status_code == 200
+    assert response.json() == []
+
+def test_sort_itemsv1_nonlist():
+    """ Test sort items"""
+    payload = {
+        "property_name": "message",
+        "context_id" : 4
+    }
+    app.dependency_overrides[extras.get_context_items] = override_get_context_items_nonlist
+    response = client.post("/extras/sort_itemsv1", json=payload, headers=extras_data.HEADERS)
+    assert response.status_code == 400
+    assert response.json().get("detail") == "Must be a list of dictionaries!"
